f9b28a795db96a3100052bb70442b101846691d1
pandeyganesh <pandeyganesha@gmail.com>
2023-05-05 20:23:59 +0530

    Added getting value and adding in list

--- frontend/script/subjects.js
+++ frontend/script/subjects.js
@@ -1,6 +1,7 @@
 function rotate() {
     const btn = document.getElementById('rotate-btn');
     const btn_div = document.getElementById('add_div');
+    const sub_list = document.getElementById('sub_list');
     const isMoved = btn.getAttribute('data-moved') == 'true';
     const textBox = document.createElement('input');
     textBox.setAttribute("type", "text");
@@ -19,12 +20,20 @@ function rotate() {
             textBox.classList.add('visible');
         }, 350);
     } else {
+
         console.log("Anticlockwise move");
         btn.style.left = '5%';
         btn.style.transform = 'rotate(0deg)';
         btn.style.background = '#EF5350';
         btn.setAttribute('data-moved', 'false');
         const inputBox = document.getElementById('new_sub');
+        var data = inputBox.value;
+        if (data!=''){
+            console.log("Running");
+            const li = document.createElement('li');
+            li.innerHTML=data;
+            sub_list.appendChild(li);
+        }
         inputBox.classList.remove('visible');
         setTimeout(() => {
             inputBox.parentNode.removeChild(inputBox);
